137ad83b72656ff32f0be6d79ee03f4436a98405
Thijs De Meester <git@tdemeester.nl>
2016-07-27 21:51:10 +0200

    add support for projectevents

---
+++ Get-GitLabProjectEvents.ps1
@@ -0,0 +1,32 @@
+﻿function Get-GitLabProjectEvents
+{
+  <#
+      .Synopsis
+      Get the events for the specified project.
+      .DESCRIPTION
+      Get the events for the specified project. Sorted from newest to latest.
+  #>
+  [CmdletBinding()]
+  [Alias()]
+  [OutputType()]
+  Param
+  (
+    # Param1 help description
+    [Parameter(ParameterSetName = 'The ID of a project',
+        HelpMessage = 'ProjectID',
+    Mandatory = $true)]
+    [Alias('ProjectID')]
+    [int]$id,
+
+    [Parameter(HelpMessage = 'Specify Existing GitlabConnector',
+        Mandatory = $false,
+    DontShow = $true)]
+    [psobject]$GitlabConnect = (Get-GitlabConnect)
+  )
+
+  $httpmethod = 'get'
+  $apiurl = "projects/$id/events"
+  $parameters = @{}
+
+  $GitlabConnect.callapi($apiurl,$httpmethod,$parameters)
+}
--- loader.psm1
+++ loader.psm1
@@ -11,6 +11,7 @@
 . $PSScriptRoot\Add-GitLabToken.ps1
 . $PSScriptRoot\Get-GitlabConnect.ps1
 . $PSScriptRoot\Get-GitLabProject.ps1
+. $PSScriptRoot\Get-GitLabProjectEvents.ps1
 . $PSScriptRoot\Get-GitLabToken.ps1
 . $PSScriptRoot\init.ps1
 . $PSScriptRoot\New-GitlabProject.ps1
