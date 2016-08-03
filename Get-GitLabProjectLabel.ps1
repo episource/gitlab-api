b099685c448780ccc80f0123b5869ded6288253b
Thijs De Meester <git@tdemeester.nl>
2016-08-03 15:23:12 +0200

    add Get-GitLabProjectLabel

---
+++ Get-GitLabProjectLabel.ps1
@@ -0,0 +1,33 @@
+﻿function Get-GitLabProjectLabel
+{
+  <#
+      .Synopsis
+      get labels
+      .DESCRIPTION
+      Get all labels for a given project.
+      .Example
+  #>
+  [CmdletBinding()]
+  [Alias()]
+  [OutputType()]
+  Param
+  (
+    # Param1 help description
+    [Parameter(ParameterSetName = 'The ID of a project',
+        HelpMessage = 'ProjectID',
+    Mandatory = $true)]
+    [Alias('ProjectID')]
+    [int]$id,
+
+    [Parameter(HelpMessage = 'Specify Existing GitlabConnector',
+        Mandatory = $false,
+    DontShow = $true)]
+    [psobject]$GitlabConnect = (Get-GitlabConnect)
+  )
+  
+  $httpmethod = 'get'
+  $apiurl = "projects/$id/labels"
+  $parameters = @{}
+
+  $GitlabConnect.callapi($apiurl,$httpmethod,$parameters)
+}
