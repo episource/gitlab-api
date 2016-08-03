b1ac1a96c7a6540733b2b1fa6cf8c9ef342636b1
Thijs De Meester <git@tdemeester.nl>
2016-08-03 15:40:10 +0200

    add newgitlabprojectlabel

---
+++ New-GitLabProjectLabel.ps1
@@ -0,0 +1,66 @@
+﻿function New-GitLabProjectLabel
+{
+  <#
+      .Synopsis
+      creates a new label
+      .DESCRIPTION
+      Creates a new label for the given project with the given name and color.
+      .Example
+  #>
+  [CmdletBinding()]
+  [Alias()]
+  [OutputType()]
+  Param
+  (
+    [Parameter(HelpMessage = 'ProjectID',
+    Mandatory = $true)]
+    [Alias('ProjectID')]
+    [int]$id,
+
+    [Parameter(HelpMessage = 'Label Name',
+    Mandatory = $true)]
+    [Alias()]
+    [string]$name,
+
+    [Parameter(HelpMessage = 'Label Color Hex notation (#000000)',
+    Mandatory = $false)]
+        [Alias('HEXColor')]
+    [ValidatePattern('^#[A-Fa-f0-9]{6}$')]
+    [string]$Color = '#428bca',
+
+    [Parameter(HelpMessage = 'Label Description',
+    Mandatory = $false)]
+    [Alias()]
+    [string]$description,
+
+    [Parameter(HelpMessage = 'Specify Existing GitlabConnector',
+        Mandatory = $false,
+    DontShow = $true)]
+    [psobject]$GitlabConnect = (Get-GitlabConnect),
+
+    
+
+        [Parameter(HelpMessage='Passthru the created project',
+                   Mandatory=$false)]
+        [switch]$PassThru
+  
+  )
+  
+  $httpmethod = 'post'
+  $apiurl = "projects/$id/labels"
+  $parameters = @{
+    id = $id
+    name = $name
+    color = $color
+    }
+
+  if($description){
+    $parameters.description = $description
+  }
+
+  $newlabel = $GitlabConnect.callapi($apiurl,$httpmethod,$parameters)
+
+  if($PassThru){
+    return $newlabel
+  }
+}
--- loader.psm1
+++ loader.psm1
@@ -20,6 +20,7 @@
 . $PSScriptRoot\Invoke-GitLabProjectUnArchive.ps1
 . $PSScriptRoot\Invoke-GitLabProjectFork.ps1
 . $PSScriptRoot\New-GitlabProject.ps1
+. $PSScriptRoot\New-GitLabProjectLabel.ps1
 . $PSScriptRoot\Remove-GitLabProjectStar.ps1
 . $PSScriptRoot\Remove-GitLabToken.ps1
 . $PSScriptRoot\Set-GitlabProject.ps1
