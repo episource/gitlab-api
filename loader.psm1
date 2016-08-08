ced4a70efe0157a89e1851797f76f7e88bd09323
Thijs De Meester <git@tdemeester.nl>
2016-08-08 15:28:33 +0200

    add new commit comment function

--- loader.psm1
+++ loader.psm1
@@ -41,6 +41,7 @@
 . $PSScriptRoot\New-GitLabProjectLabel.ps1
 . $PSScriptRoot\New-GitLabProjectMergeRequest.ps1
 . $PSScriptRoot\New-GitLabProjectMilestone.ps1
+. $PSScriptRoot\New-GitLabProjectRepositoryCommitComment.ps1
 . $PSScriptRoot\New-GitLabProjectRepositoryTag.ps1
 . $PSScriptRoot\Protect-GitlabProjectBranch.ps1
 . $PSSCriptRoot\Register-GitLabProjectLabelSubscription.ps1
