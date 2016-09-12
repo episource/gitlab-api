077ba1db8d6b25d82e4306fe464690d71dc56d05
Thijs De Meester <git@tdemeester.nl>
2016-09-12 11:49:45 +0200

    fix new-gitlabproject
    splitsed parameterhandling.
    this allowed for better parameter conversion.

--- New-GitlabProject.ps1
+++ New-GitlabProject.ps1
@@ -84,9 +84,112 @@
     $httpmethod = 'post'
     $apiurl = 'projects'
     $parameters = @{}
-    $boundparameters = $PSCmdlet.MyInvocation.BoundParameters.keys.where({-not ($_ -in 'GitLabConnect','PassThru')})
-    foreach($boundparameter in $boundparameters){
-        $parameters.$boundparameter = [string]$PSCmdlet.MyInvocation.BoundParameters.$boundparameter
+     $parameters =@{}
+    #name
+    if($name){
+      $parameters.'name' = $name
+    }
+    #path
+    if($path){
+      $parameters.path = $path
+    }
+    #default_branch
+    if($default_branch){
+      $parameters.'default_branch' = $default_branch
+    }
+    #description
+    if($PSCmdlet.MyInvocation.BoundParameters.keys -contains 'description'){
+      if($description){
+        $parameters.description = 'true'
+      }
+      else{
+        $parameters.description = 'false'
+      }
+    }
+    #issues_enabled
+    if($PSCmdlet.MyInvocation.BoundParameters.keys -contains 'issues_enabled'){
+      if($issues_enabled){
+        $parameters.issues_enabled = 'true'
+      }
+      else{
+        $parameters.issues_enabled = 'false'
+      }
+    }
+    #merge_requests_enabled
+    if($PSCmdlet.MyInvocation.BoundParameters.keys -contains 'merge_requests_enabled'){
+      if($merge_requests_enabled){
+        $parameters.merge_requests_enabled = 'true'
+      }
+      else{
+        $parameters.merge_requests_enabled = 'false'
+      }
+    }
+    #builds_enabled
+    if($PSCmdlet.MyInvocation.BoundParameters.keys -contains 'builds_enabled'){
+      if($builds_enabled){
+        $parameters.builds_enabled = 'true'
+      }
+      else{
+        $parameters.builds_enabled = 'false'
+      }
+    }
+    #wiki_enabled
+    if($PSCmdlet.MyInvocation.BoundParameters.keys -contains 'wiki_enabled'){
+      if($wiki_enabled){
+        $parameters.wiki_enabled = 'true'
+      }
+      else{
+        $parameters.wiki_enabled = 'false'
+      }
+    }
+    #snippets_enabled
+    if($PSCmdlet.MyInvocation.BoundParameters.keys -contains 'snippets_enabled'){
+      if($snippets_enabled){
+        $parameters.snippets_enabled = 'true'
+      }
+      else{
+        $parameters.snippets_enabled = 'false'
+      }
+    }
+    #container_registry_enabled
+    if($PSCmdlet.MyInvocation.BoundParameters.keys -contains 'container_registry_enabled'){
+      if($container_registry_enabled){
+        $parameters.container_registry_enabled = 'true'
+      }
+      else{
+        $parameters.container_registry_enabled = 'false'
+      }
+    }
+    #shared_runners_enabled
+    if($PSCmdlet.MyInvocation.BoundParameters.keys -contains 'shared_runners_enabled'){
+      if($shared_runners_enabled){
+        $parameters.shared_runners_enabled = 'true'
+      }
+      else{
+        $parameters.shared_runners_enabled = 'false'
+      }
+    }
+    #visibility_level
+    if($visibility_level){
+      $parameters.'visibility_level' = $visibility_level
+    }
+    #public
+    if($PSCmdlet.MyInvocation.BoundParameters.keys -contains 'public'){
+      if($public){
+        $parameters.public = 'true'
+      }
+      else{
+        $parameters.public = 'false'
+      }
+    }
+    #public_builds
+    if($PSCmdlet.MyInvocation.BoundParameters.keys -contains 'public_builds'){
+      if($public_builds){
+        $parameters.public_builds = 'true'
+      }
+      else{
+        $parameters.public_builds = 'false'
+      }
     }
 
     $newproj = $GitlabConnect.callapi($apiurl,$httpmethod,$parameters)
